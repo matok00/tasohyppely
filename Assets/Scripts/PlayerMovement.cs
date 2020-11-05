34fc3658601e6c663f628278af09e28e3a17e7cd
matok00 <mattimelnikov@gmail.com>
2020-11-05 19:28:18 +0200

    scripti aloitettu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public BoxCollider2D myBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRigidBody = GetComponent<Rigidbody2D>();
+       myBody = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
